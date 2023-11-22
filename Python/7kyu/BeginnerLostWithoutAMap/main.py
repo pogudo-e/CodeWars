4c77807db3431904f1b76ab0e6921bd33e0fa9aa
Pogudo Evgeny <sensesbit@gmail.com>
2023-11-22 20:55:11 +0300

    append ^-^

---
+++ Python/7kyu/BeginnerLostWithoutAMap/main.py
@@ -0,0 +1,7 @@
+def maps(a: list) -> list:
+    return [m * 2 for m in a]
+
+
+print(maps([1, 2, 3]))
+print(maps([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(maps([]))
---
+++ Python/8kyu/TwiceAsOld/main.py
@@ -0,0 +1,5 @@
+def twice_as_old(dad_years_old: int, son_years_old: int) -> int:
+    return abs(son_years_old * 2 - dad_years_old)
+
+
+print(twice_as_old(36, 7))
