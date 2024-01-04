534262f318066c20eeed3d10f3dab8345d0f8a84
Pogudo Evgeny <sensesbit@gmail.com>
2024-01-04 21:46:33 +0300

    add 8 kyu kata determinate off spring sex based n xx and xy chromosomes

---
+++ Python/8kyu/DetermineOffspringSexBasedOnGenesXxAndXyChromosomes/main.py
@@ -0,0 +1,3 @@
+def chromosome_check(chromosome):
+    return 'Congratulations! You\'re going to have a son.' if 'Y' in chromosome \
+        else 'Congratulations! You\'re going to have a daughter.'
