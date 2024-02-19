6aedb2c973cd281dcf9c3c8dfa847a5817f100cc
Pogudo Evgeny <sensesbit@gmail.com>
2024-02-19 22:02:15 +0300

    add 7 kyu kata regex validate pin code

---
+++ Python/7kyu/RegexValidatePinCode/main.py
@@ -0,0 +1,7 @@
+def validate_pin(pin: str):
+    if len(pin) == 4 or len(pin) == 6:
+        for i in pin:
+            if not i.isdigit():
+                return False
+        return True
+    return False
